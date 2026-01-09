c583fb389b4c36fbb6c923a3cfce2a3d1f599c26
Kenny Root <kenny@the-b.org>
2026-01-08 22:22:09 -0800

    feat(fonts): optimize font loading
    Optimize font loading to reduce first contentful paint and the last
    contentful paint lighthouse metrics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import type { FC, ReactNode } from 'react';
 
+import { Raleway, Source_Sans_3 } from 'next/font/google';
+import localFont from 'next/font/local';
 import {
   LastUpdated,
   Layout,
@@ -10,6 +12,32 @@ import { Head } from 'nextra/components';
 import { getPageMap } from 'nextra/page-map';
 import themeConfig from '@/theme.config';
 import './styles.css';
+
+const raleway = Raleway({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-raleway',
+});
+
+const sourceSans = Source_Sans_3({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-source-sans',
+});
+
+const datatypeThin = localFont({
+  src: [
+    {
+      path: '../../public/fonts/logo-font.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-datatype-thin',
+});
 
 export const metadata: Metadata = {
   description:
@@ -45,7 +73,7 @@ const RootLayout: FC<LayoutProps> = async ({ children }) => {
   );
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${raleway.variable} ${sourceSans.variable} ${datatypeThin.variable}`}>
       <Head
         backgroundColor={{
           dark: 'rgb(15,23,42)',
